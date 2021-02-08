920d5a8a8568b6077318217f79c4f5b79ad7e028
hansputera <hanifdwyputrasembiring@gmail.com>
2021-02-08 14:41:00 +0700

    a bit changes

--- webset/libs/config.php
+++ webset/libs/config.php
@@ -2,6 +2,6 @@
     $discord_api_url = "https://discord.com/api/v6";
 
     // Discord configuration
-    $clientID = "804179465399042098";
-    $clientSecret = "NAklsjcRwg9QuoM3m9mcT4PaIhKEc2g4";
+    $clientID = "";
+    $clientSecret = "";
 ?>
